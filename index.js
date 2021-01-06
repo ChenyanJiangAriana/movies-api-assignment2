f4dede3b02f025f7d2a2b123e00261dd45a677e9
Chenyan <624071310@qq.com>
2021-01-06 00:04:04 +0000

    add mongoose

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import bodyParser from 'body-parser';
 import './db';
+import {loadUsers} from './seedData'
 
 dotenv.config();
 
@@ -9,8 +11,26 @@ const app = express();
 
 const port = process.env.PORT;
 
+if (process.env.SEED_DB) {
+  loadUsers();
+}
+
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
+};
+//configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
 app.use(express.static('public'));
-app.use('/api/movies',moviesRouter);
+app.use('/api/movies', moviesRouter);
+
+app.use(errHandler);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
